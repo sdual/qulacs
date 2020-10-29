f239779f005db4dea91c9065c5405ff590c03258
sdual <shr.nrht.0623@gmail.com>
2020-10-30 03:52:25 +0900

    add settings to build julia wrappers

---
+++ src/julia/cppsim/state_wrapper.cpp
@@ -0,0 +1,13 @@
+#include <jlcxx/jlcxx.hpp>
+#include "cppsim/state.hpp"
+
+namespace jlcxx
+{
+    template<> struct SuperType<QuantumStateCpu> { typedef QuantumStateBase type; };
+}
+
+JLCXX_MODULE define_types_module(jlcxx::Module& quantum_state_types)
+{
+      quantum_state_types.add_type<QuantumStateCpu>("QuantumStateCpu", jlcxx::julia_base_type<QuantumStateBase>());
+      quantum_state_types.constructor<UINT>();
+}

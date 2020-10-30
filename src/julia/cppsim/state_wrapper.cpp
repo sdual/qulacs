7c95c6cca67e3aa92189052962b9c73c75efc2c4
sdual <shr.nrht.0623@gmail.com>
2020-10-30 13:41:35 +0900

    fix CMakeLists.txt

--- src/julia/cppsim/state_wrapper.cpp
+++ src/julia/cppsim/state_wrapper.cpp
@@ -8,6 +8,6 @@ namespace jlcxx
 
 JLCXX_MODULE define_types_module(jlcxx::Module& quantum_state_types)
 {
-      quantum_state_types.add_type<QuantumStateCpu>("QuantumStateCpu", jlcxx::julia_base_type<QuantumStateBase>());
-      quantum_state_types.constructor<UINT>();
+      quantum_state_types.add_type<QuantumStateCpu>("QuantumStateCpu", jlcxx::julia_base_type<QuantumStateBase>())
+          .constructor<UINT>();
 }
